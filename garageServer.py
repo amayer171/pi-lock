8fd8a421ee8ad4b4f669b31560a87c5ad47bcc0e
Andy Mayer <amayer@novacoast.com>
2017-04-09 23:55:15 +0000

    working door opener

--- garageServer.py
+++ garageServer.py
@@ -1,14 +1,17 @@
 import http.server
 import socketserver
 import json
+from garage_gpio import PiPin
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        print("do Get") 
         #get path from the url ie: '/close' 
         path = (self.path.lower())
 
         result = self.processRequest(path) 
-       
+        print("processed request") 
+        
         #send the response:
         self.send_response(result['response'])
         self.send_header("Content-type", 'application/json' )
@@ -20,20 +23,32 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.wfile.write(bytes(json.dumps(result), 'UTF-8') )
     
     def processRequest(self, path):
-        result = self.success(path) 
-        return result
+        print("process request for path: " + path)
+
+        if path == "/door":
+
+            pin = PiPin() 
+            pin.send_signal()
+
+            result = self.success(path) 
+            return result
+        else:
+            return self.unknownEndpoint(path)
 
     def success(self, action):
         return { 'response': 200, 'action': action, 'result': 'success' }
 
     def unknownEndpoint(self, action):
         return { 'response': 404, 'action': action, 'result': 'unknown endpoint' } 
-
+    
 PORT = 8001
+try:
+    socketserver.TCPServer.allow_reuse_address = True
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    print("serving at port", PORT)
+    httpd.serve_forever()
 
-myHandler = Handler
-
-socketserver.TCPServer.allow_reuse_address = True
-httpd = socketserver.TCPServer(("", PORT), myHandler)
-print("serving at port", PORT)
-httpd.serve_forever()
+except:
+    PiPin().cleanup()
+    print("Application has ended and cleaned up successfully")
+    
--- garage_gpio.py
+++ garage_gpio.py
@@ -1,31 +1,44 @@
 import time
 import RPi.GPIO as GPIO
+import sys
 
 class PiPin():
     CONST_PIN = 21
-    SLEEP_SECONDS = 1.5
+    SLEEP_SECONDS = 2.5
     def send_signal(self):
         self.setup()
-        GPIO.output(self.CONST_PIN, GPIO.HIGH)
-        time.sleep(self.SLEEP_SECONDS)
         GPIO.output(self.CONST_PIN, GPIO.LOW)
-        GPIO.cleanup()
+        print("LOW")
+        time.sleep(self.SLEEP_SECONDS)
+        GPIO.output(self.CONST_PIN, GPIO.HIGH)
+        print("HIGH")
         
     def setup(self):
         #GPIO.setmode(GPIO.BOARD)
         GPIO.setmode(GPIO.BCM) #BCM (numbers on breakout board) or BOARD (linear numbering of pins)
-        GPIO.setup(self.CONST_PIN, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.CONST_PIN, GPIO.OUT, initial=GPIO.HIGH)
+
+    def cleanup(self):
+        GPIO.cleanup()
 	    
 if __name__ == "__main__":
     # execute only if run as a script
     pin = PiPin()
-    try:	
+    try:
+        pin.setup()
+        GPIO.setup(20, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(16, GPIO.LOW)
+        
+        	
         while(True):
-            time.sleep(2)
-            print("signaling...")
+            print("Press any key to send signal.")
+            sys.stdin.readline()
+    
             pin.send_signal() 
             print("done.")
     
-    except KeyboardInterrupt:
-        GPIO.cleanup() 
-        print ("\nKeyboard Interrupt detected\n")
+    except:
+        pin.cleanup() 
+        print("cleaned up after exception.")
